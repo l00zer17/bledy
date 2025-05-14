de26674f359418d994f8c190de3658001f9caf4f
l00zer17 <Lukasz.tabor4@gmail.com>
2025-05-14 13:59:20 +0000

    6_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,8 @@
-/*
-Program sprawdza poprawność wpisywanego imienia. W przypadku wystąpienia spacji w imieniu, funkcja wyrzuca zdefiniowany wyjątek WrongStudentName, który jest wyłapywany w pętli głównej Commit6_0.
-Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces modyfikacji ogólnie może wyglądać następująco:
-• Ustalenie jaki błąd chcę się sprawdzić i wyłapać.
-• Decyzja, czy użyje się własnej klasy wyjątku, czy wykorzysta już istniejące (np. Exception, IOException).
-• Napisanie kodu sprawdzającego daną funkcjonalność. W przypadku warunku błędu wyrzucany będzie wyjątek: throw new WrongStudentName().
-• W definicji funkcji, która zawiera kod wyrzucania wyjątku dopisuje się daną nazwę wyjątku, np. public static String ReadName() throws WrongStudentName.
-• We wszystkich funkcjach, które wywołują powyższą funkcję także należy dopisać, że one wyrzucają ten wyjątek – inaczej program się nie skompiluje.
-• W pętli głównej, w main’ie, w zdefiniowanym już try-catch dopisuje się Nazwę wyjątku i go obsługuje, np. wypisuje w konsoli co się stało.
-*/
-
-//Commit6_1. Na podstawie analogii do wyjątku WrongStudentName utwórz i obsłuż wyjątki WrongAge oraz WrongDateOfBirth. 
-//Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
-
 import java.io.IOException;
 import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
+class WrongAge extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -34,6 +21,8 @@ class Main {
 
             } catch(WrongStudentName e) {
                 System.out.println("Błędne imię studenta!");
+            } catch(WrongAge e) {
+                System.out.println("Błędny wiek studenta! Wiek musi być z przedziału 1–99 lat.");
             }
         }
     }
@@ -53,17 +42,21 @@ class Main {
         String name = scan.nextLine();
         if(name.contains(" "))
             throw new WrongStudentName();
-
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName {
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge {
         var name = ReadName();
+
         System.out.println("Podaj wiek: ");
         var age = scan.nextInt();
+        if(age <= 0 || age >= 100)
+            throw new WrongAge();
+
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYYY");
         var date = scan.nextLine();
+
         (new Service()).addStudent(new Student(name, age, date));
     }
 
